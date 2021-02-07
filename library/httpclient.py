b6326b6e6d9bdfb383978f53e0b10f2aa9818b5b
hm1sty <1578259864@qq.com>
2021-02-07 14:32:23 +0800

    Update httpclient.py

--- library/httpclient.py
+++ library/httpclient.py
@@ -27,6 +27,6 @@ class HttpClient:
             response = self.client.get(url, headers=headers, data=data, json=json, params=params
                                        , timeout=self.timeout, *args, **kwargs)
         response.encoding = 'utf-8'
-        print(f'{response.json()}')
+#         print(f'{response.json()}')
 
         return response
